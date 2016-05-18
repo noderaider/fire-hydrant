1fa0aad30d7c11fc4d0a61cd32bdd5b63ff79b95
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-05-18 16:27:46 -0700

    fixed initial state

--- src/lib/react/createInitialState.js
+++ src/lib/react/createInitialState.js
@@ -1,10 +1,13 @@
-import { serializer } from '../'
-
 export default function createInitialState({ React, ...deps }) {
-  return ({ serialize = serializer.serialize, stateKey = '__initialState__', globalKey, state }) => {
+  const { PropTypes } = React
+  const InitialState = ({ serialize, stateKey = '__initialState__', globalKey, state }) => {
+
     const serialized = serialize(state, deps)
     const baseKey = globalKey ? `window.${globalKey}=window.${globalKey} || {};window.${globalKey}` : 'window'
     const __html = `${baseKey}.${stateKey}=${serialized}`
     return <script dangerouslySetInnerHTML={{ __html }} />
   }
+  InitialState.propTypes = { serialize: PropTypes.func.isRequired }
+  return InitialState
 }
+
